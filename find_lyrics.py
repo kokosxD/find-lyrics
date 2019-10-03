389c654cea5a4d69a661885a502efc586242074c
kokosxD <kokos1811@outlook.com.gr>
2019-10-03 04:00:05 +0300

    1.4

--- find_lyrics.py
+++ find_lyrics.py
@@ -5,13 +5,13 @@ song = input("Enter song: ")
 url_params = {"q": song.lower()}
 # Making a get request to the azlyric's search ulr + passing the above parameters to it
 azlyrics_search = rq.get("https://search.azlyrics.com/search.php", params=url_params)
-soup_1 = bs(azlyrics_search.text, "lxml")
+soup = bs(azlyrics_search.text, "lxml")
 # Get a list with all the results for the specific search
 search_results = soup_1.find_all("table", {"class": "table table-condensed"})[-1].find_all("td", {"class": "text-left visitedlyr"})
 # Take the first result from the search as the url with the lyrics on it
 lyrics_url = search_results[0].a.get("href")
 lyrics_page = rq.get(lyrics_url)
-soup_2 = bs(lyrics_page.text, "lxml")
+soup = bs(lyrics_page.text, "lxml")
 # Find the lyrics
 lyrics = soup_2.find("div", {"class": "col-xs-12 col-lg-8 text-center"}).find_all("div")[6].get_text()
 # Make a list with every line of the lyrics as a element and remove the first 2 because are html notes
