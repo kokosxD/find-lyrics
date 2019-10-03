4b975fa3edce06827fbf15c32dd386797bc8571c
kokosxD <kokos1811@outlook.com.gr>
2019-10-03 07:38:21 +0300

    1.5

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 This is a program I made for finding the lyrics of a song based on AZLyrics.
-Here is how it works:
 
-・Before you run it, change the "song" variable to a song you want to find the lyrics for.
-
-・It will search on AZLyrics for this specific song and print
-the lyrics for the top result based on the given song's name. If the song you
-searched for is not that popular and they are other songs with the same name,
-it may not print the right lyrics.
+・It will search on AZLyrics for a specific song and print the lyrics for
+the top result based on the given song's name.
 
 ・For more info about how it works you can see find_lyrics.py.
 
--- find_lyrics.py
+++ find_lyrics.py
@@ -1,19 +1,25 @@
 import requests as rq
 from bs4 import BeautifulSoup as bs
 song = input("Enter song: ")
+
 # Making the parameters for the url
 url_params = {"q": song.lower()}
+
 # Making a get request to the azlyric's search ulr + passing the above parameters to it
 azlyrics_search = rq.get("https://search.azlyrics.com/search.php", params=url_params)
 soup = bs(azlyrics_search.text, "lxml")
+
 # Get a list with all the results for the specific search
 search_results = soup_1.find_all("table", {"class": "table table-condensed"})[-1].find_all("td", {"class": "text-left visitedlyr"})
+
 # Take the first result from the search as the url with the lyrics on it
 lyrics_url = search_results[0].a.get("href")
 lyrics_page = rq.get(lyrics_url)
 soup = bs(lyrics_page.text, "lxml")
+
 # Find the lyrics
 lyrics = soup_2.find("div", {"class": "col-xs-12 col-lg-8 text-center"}).find_all("div")[6].get_text()
+
 # Make a list with every line of the lyrics as a element and remove the first 2 because are html notes
 lines = lyrics.splitlines()[2:]
 processed_lyrics = "\n".join(lines)
