1b993e6b78742fa62cecba2695f89f007e4d2601
kokosxD <kokos1811@outlook.com.gr>
2019-11-01 00:05:23 +0200

    2.0

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-This is a program I made for finding the lyrics of a song based on AZLyrics.
+### This is a program I made for finding the lyrics of a song based on AZLyrics
 
-・It will search on AZLyrics for a specific song and print the lyrics for
-the top result based on the given song's name.
+---
 
-・For more info about how it works you can see find_lyrics.py.
+* It will search on **AZLyrics** for a specific song and print the **lyrics** for the top result based on the given song's name.
 
-Feel free to use it :)
+* For more info about how it works you can see find_lyrics.py.
+
+---
+
+You can either run it from **Command Prompt**
+
+```
+py find_lyrics.py
+```
+
+* See more [here](example.md "example.md")
+
+Or import it and use it as a **library**
+
+```python
+from find_lyrics import find_lyrics
+song = "Numb"
+lyrics, search_time = find_lyrics(song)
+```
+
+**NOTE:** The searching may take a while since the site's type is dynamic and the lyrics are loading after the main page does.
+
+Feel free to use it **:)**
--- find_lyrics.py
+++ find_lyrics.py
@@ -1,26 +1,56 @@
 import requests as rq
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as bs
-song = input("Enter song: ")
+from time import time
+def find_lyrics(song=None):
+	try:
+		if song == None:
+			song = input("Enter song: ")
 
-# Making the parameters for the url
-url_params = {"q": song.lower()}
+		print("\nSearching for \"%s\"...\n" % (song))
+		t_bf = time()
 
-# Making a get request to the azlyric's search ulr + passing the above parameters to it
-azlyrics_search = rq.get("https://search.azlyrics.com/search.php", params=url_params)
-soup = bs(azlyrics_search.text, "lxml")
+		# Making the parameters for the url
+		url_params = {"q": song.lower()}
+		options = Options()
+		options.add_argument("--headless")
+		options.add_argument("--disable-extensions")
+		options.add_argument("log-level=3")
+		options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-# Get a list with all the results for the specific search
-search_results = soup.find_all("table", {"class": "table table-condensed"})[-1].find_all("td", {"class": "text-left visitedlyr"})
+		# Getting the search url
+		search_url = rq.get("https://search.azlyrics.com/search.php", params=url_params).url
+		driver = webdriver.Chrome(options=options)
 
-# Take the first result from the search as the url with the lyrics on it
-lyrics_url = search_results[0].a.get("href")
-lyrics_page = rq.get(lyrics_url)
-soup = bs(lyrics_page.text, "lxml")
+		# Making a get request to the azlyric's
+		driver.get(search_url)
+		azlyrics_search = driver.page_source
+		driver.quit()
+		soup = bs(azlyrics_search, "lxml")
 
-# Find the lyrics
-lyrics = soup.find("div", {"class": "col-xs-12 col-lg-8 text-center"}).find_all("div")[6].get_text()
+		# Get a list with all the results for the specific search
+		search_results = soup.find_all("table", {"class": "table table-condensed"})[-1].find_all("td", {"class": "text-left visitedlyr"})
 
-# Make a list with every line of the lyrics as a element and remove the first 2 because are html notes
-lines = lyrics.splitlines()[2:]
-processed_lyrics = "\n".join(lines)
-print(processed_lyrics)
+		# Take the first result from the search as the url with the lyrics on it
+		lyrics_url = search_results[0].a.get("href")
+		lyrics_page = rq.get(lyrics_url)
+		soup = bs(lyrics_page.text, "lxml")
+
+		# Find the lyrics
+		lyrics = soup.find("div", {"class": "col-xs-12 col-lg-8 text-center"}).find_all("div")[5].get_text()
+
+		# Make a list with every line of the lyrics as a element and remove the first 2 because are html notes
+		lines = lyrics.splitlines()[2:]
+		processed_lyrics = "\n".join(lines)
+
+		# Search time (in seconds)
+		t_af = time()
+		search_time = round(t_af - t_bf, 1)
+
+		return processed_lyrics, search_time
+	except:
+		return "\"%s\" Not Found..." % (song)
+
+if __name__ == "__main__":
+	print(find_lyrics()[0])
